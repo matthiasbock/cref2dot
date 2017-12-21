e6079131654008e77c5dafc4e1386719f765331e
Matthias Bock <mail@matthiasbock.net>
2017-12-21 15:14:53 +0100

    Added possibilities to automatically simplify the graph

--- cref2dot.py
+++ cref2dot.py
@@ -28,7 +28,7 @@ from networkx import MultiDiGraph, draw
 #
 # Find cross reference table in linker map file
 #
-def find_cref(inmap):
+def find_cref_table(inmap):
     while True:
         l = inmap.readline()
         if l.strip() == 'Cross Reference Table':
@@ -42,27 +42,84 @@ def find_cref(inmap):
             if words[0] == 'Symbol' and words[1] == 'File':
                 return True
 
+
 #
 # Interpret a cross reference table
 #
-def read_cref(inmap):
+def read_cref_table(
+        inmap,
+        strip_paths=["/usr", "lib/STM32Cube_FW_F4_V1.16.0"],
+        simplify_paths=["libgcc.a", "libc_nano.a", "libnosys.a"],
+        hide_aeabi_symbols=True
+        ):
+
+    # Helper function to simplify filenames in order to increase graph clarity
+    def process_filename(last_module):
+        # Strip certain file paths
+        for key in strip_paths:
+            if last_module.find(key) > -1:
+                s = last_module.split("/")
+                print(last_module + " - " + s[len(s)-1])
+                last_module = s[len(s)-1]
+        # Remove symbol names (in round brackets) from certain libraries
+        for key in simplify_paths:
+            if last_module.find(key) > -1:
+                s = last_module.split("(")
+                print(last_module + " - " + s[0])
+                last_module = s[0]
+        return last_module
+
+    # Create new graph
     modules = MultiDiGraph()
+
+    # Parse all table lines
+    redundant_associations = []
     while True:
         l = inmap.readline()
         words = l.split()
+
         if len(words) == 2:
+            # New symbol A, which is defined in file B
             last_symbol = words[0]
-            last_module = words[1]
+            last_module = process_filename(words[1])
+
         elif len(words) == 1:
-            modules.add_edge(words[0], last_module, label=last_symbol);
+            # One more file uses the previous symbol
+            filename = process_filename(words[0])
+
+            # References of a file to itself are not particularly interesting
+            if filename != last_module:
+                if last_symbol.find("__aeabi") > -1 and hide_aeabi_symbols:
+                    t = (filename, last_module)
+                    if not (t in redundant_associations):
+                        redundant_associations.append(t)
+                        modules.add_edge(filename, last_module);
+                else:
+                    modules.add_edge(filename, last_module, label=last_symbol);
+
         elif len(l) == 0:
+            # End of table
             break
+
+    # Return the generated graph
     return modules
 
-# when this file is executed
+
+#
+# Invoke dot to export dot-file to PDF
+#
+def dot_to_pdf(dot_filename, pdf_filename):
+    print("Using dot to export " + dot_filename + " to "+pdf_filename + "...")
+    Popen(split("dot " + dot_filename + "-Tpdf " + pdf_filename)).wait()
+    print("PDF export completed.")
+
+
+#
+# Main program
+#
 if __name__ == "__main__":
     if len(argv) != 3:
-        print "Usage: "+argv[0]+" <linker.map> <output.dot>"
+        print "Usage: "+argv[0]+" <linker.map> <output.dot/pdf>"
         exit()
 
     infile = argv[1]
@@ -70,11 +127,17 @@ if __name__ == "__main__":
 
     print "Reading "+infile+" ..."
     inmap = open(infile, 'r')
-    if find_cref(inmap):
-        graph = read_cref(inmap)
+    if find_cref_table(inmap):
+        graph = read_cref_table(inmap)
         draw(graph)
         print "Writing "+outfile+" ..."
         write_dot(graph, outfile)
+
+        #if dot_filename[-4:] != ".dot":
+        #    print("Unknown file extension. Should be '.dot'.")
+        #    return
+        #pdf_filename = dot_filename[-4:] + ".pdf"
+
     else:
         print "Error: Cross reference table not found in map file."
 
